70c6061ceab1c4b7f04a6f23e098367dc78b95a6
AlbertBaluyot <albertbaluyot09@gmail.com>
2023-09-23 12:51:23 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "BABYCAKES",
+    introTitle: 'TE-A-MO',
+    introDesc: `“I saw that you were perfect, and so I loved you. Then I saw that you were not perfect and I loved you even more.”!`,
+    btnIntro: '^^CLICK ME^^',
+    title: 'Love isn’t something you find. Love is something that finds you. 🥰',
+    desc: 'Smile Kalang🥰🥰🥰!! ',
+    btnYes: 'Yes 🥰',
+    btnNo: 'No 😭',
+    question: 'Kaya mo yan Laban lang 🥰',
+    btnReply: 'CLICK ME PLSSS 😭',
+    reply: 'Yes 😘😘🥺 ',
+    mess: 'Wag Susuko🥺🥺',
+    messDesc: ' Smile Kalang Palagi Andito lang ako para Alalayan ka😘.',
+    btnAccept: 'PROMISEE🥺🥺',
+    messLink: 'https://github.com/Uzumakinaruto08' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
